e6bbb56ac6940cd8973924b17629207f2437863a
unknown <n1507@CL37G202.sit.nyp.edu.sg>
2016-08-23 09:55:53 +0800

    Delete the extra files

--- Puzzle.h
+++
@@ -1,9 +0,0 @@
-#include "Framework\timer.h"
-#include <iostream>
-
-int Puzzle();
-int random_alphabet(int a);
-int random_number_game(int a);
-int random_pattern(int a, int b);
-int Riddles(int a);
-int logic_game(int a);
--- endMenu.h
+++
@@ -1,9 +0,0 @@
-#include "SP1Framework\Framework\console.h"
-#include <fstream>
-#include <string>
-
-using std::ifstream;
-using std::string;
-using std::getline;
-
-void endMenu(Console &refCon);
