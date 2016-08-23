76c9e345b2d6e44b3ed7c68287cbf7c3c34e2f86
unknown <n1507@CL37G202.sit.nyp.edu.sg>
2016-08-23 15:16:41 +0800

    Added the alphabets

--- SP1Framework/game.h
+++ SP1Framework/game.h
@@ -14,24 +14,82 @@ extern bool g_bQuitGame;
 
 const COORD ScreenReso = { 120, 50 };
 
+#define VK_A              0x41
+#define VK_B              0x42
+#define VK_C              0x43
+#define VK_D              0x44
+#define VK_E              0x45
+#define VK_F              0x46
+#define VK_G              0x47
+#define VK_H              0x48
+#define VK_I              0x49
+#define VK_J              0x4A
+#define VK_K              0x4B
+#define VK_L              0x4C
+#define VK_M              0x4D
+#define VK_N              0x4E
+#define VK_O              0x4F
+#define VK_P              0x50
+#define VK_Q              0x51
+#define VK_R              0x52
+#define VK_S              0x53
+#define VK_T              0x54
+#define VK_U              0x55
+#define VK_V              0x56
+#define VK_W              0x57
+#define VK_X              0x58
+#define VK_Y              0x59
+#define VK_Z              0x5A
+
+
 // Enumeration to store the control keys that your game will have
 enum EKEYS
 {
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    K_SPACE,
-    K_COUNT
+	K_UP,
+	K_DOWN,
+	K_LEFT,
+	K_RIGHT,
+	K_ESCAPE,
+	K_SPACE,
+	K_COUNT
+};
+
+enum EALPHABETS
+{
+	K_A,
+	K_B,
+	K_C,
+	K_D,
+	K_E,
+	K_F,
+	K_G,
+	K_H,
+	K_I,
+	K_J,
+	K_K,
+	K_L,
+	K_M,
+	K_N,
+	K_O,
+	K_P,
+	K_Q,
+	K_R,
+	K_S,
+	K_T,
+	K_U,
+	K_V,
+	K_W,
+	K_X,
+	K_Y,
+	K_Z
 };
 
 // Enumeration for the different screen states
 enum EGAMESTATES
 {
-    S_SPLASHSCREEN,
-    S_GAME,
-    S_COUNT,
+	S_SPLASHSCREEN,
+	S_GAME,
+	S_COUNT,
 	S_PUZZLE,
 	S_PICTURES,
 	S_ENDMENU
@@ -46,15 +104,15 @@ enum CharState
 // struct for the game character
 struct SGameChar
 {
-    COORD m_cLocation;
-    bool  m_bActive;
+	COORD m_cLocation;
+	bool  m_bActive;
 };
 
-void init        ( void );      // initialize your variables, allocate memory, etc
-void getInput    ( void );      // get input from player
-void update      ( double dt ); // update the game and the state of the game
-void render      ( void );      // renders the current state of the game to the console
-void shutdown    ( void );      // do clean up, free memory
+void init(void);      // initialize your variables, allocate memory, etc
+void getInput(void);      // get input from player
+void update(double dt); // update the game and the state of the game
+void render(void);      // renders the current state of the game to the console
+void shutdown(void);      // do clean up, free memory
 
 void splashScreen();
 void splashScreenWait();    // waits for time to pass in splash screen
@@ -75,5 +133,6 @@ void RunPictures();
 void ScoreDisplay();
 void detectMazeEnd();
 void endScreen();
+void Alphabets();
 
 #endif // _GAME_H
