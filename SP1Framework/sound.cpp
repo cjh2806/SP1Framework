5e083322cd74c9e08ef287d28c860ce4e22658de
unknown <n1507@CL37G203.sit.nyp.edu.sg>
2016-08-29 11:06:17 +0800

    changed music

--- SP1Framework/sound.cpp
+++ SP1Framework/sound.cpp
@@ -1,6 +1,6 @@
 #include "sound.h"
 
-void openMusic() { mciSendString(L"open \"Sound/Nyan.mp3\" type mpegvideo alias mp3", NULL, 0, NULL); }
+void openMusic() { mciSendString(L"open \"Sound/Music.mp3\" type mpegvideo alias mp3", NULL, 0, NULL); }
 void playMusic() { mciSendString(L"play mp3 repeat", NULL, 0, NULL); }
 void pauseMusic() { mciSendString(L"pause mp3", NULL, 0, NULL); }
 void resumeMusic() { mciSendString(L"resume mp3", NULL, 0, NULL); }
